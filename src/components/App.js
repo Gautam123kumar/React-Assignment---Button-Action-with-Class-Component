bc30403c64d66868eb4d9118e28ae8dd96db9255
gautam123kumar <kumargautam9518@gmail.com>
2021-02-17 17:15:11 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,22 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+		this.state = ({
+			renderp : false,
+		})
 	};
 
     render() {
     	return(
     		<div id="main">
 				{ /* Do not remove this main div!! */ }
+				{this.state.renderp ? <p id ="para">"Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"</p>
+				: <button id ="click" onClick={()=>{
+					this.setState({
+						renderp:true,
+					});
+				}}>Click</button>
+				}
     		</div>
     	);
     }
