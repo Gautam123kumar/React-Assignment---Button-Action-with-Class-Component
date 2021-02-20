f7d57ee92aea71b3046e9631c409be15c1e3385c
gautam123kumar <kumargautam9518@gmail.com>
2021-02-20 21:55:51 +0530

    updateapp

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,8 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
-    constructor(props) {
-		super(props);
+    constructor() {
+		super();
 		this.state = ({
 			renderp : false,
 		})
@@ -13,13 +13,17 @@ class App extends Component {
     	return(
     		<div id="main">
 				{ /* Do not remove this main div!! */ }
-				{this.state.renderp ? <p id ="para">"Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"</p>
-				: <button id ="click" onClick={()=>{
-					this.setState({
-						renderp:true,
-					});
-				}}>Click</button>
-				}
+				
+				{this.state.renderP ?
+		         <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+    
+				:
+					<button id = "click" onClick={()=>{
+						this.setState({
+							renderP : true,
+						});
+					}}>Click!</button>
+				}	
     		</div>
     	);
     }
